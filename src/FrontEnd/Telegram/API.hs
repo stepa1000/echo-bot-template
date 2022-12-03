30cd1a82f31b38cf690d1e7508bf2e983d3d147a
stepan <stepa100023@gmail.com>
2022-12-03 20:10:48 +0300

    resolv problrm for lts manager
    adding data types for telegram frontend

--- src/FrontEnd/Telegram/API.hs
+++ src/FrontEnd/Telegram/API.hs
@@ -4,42 +4,28 @@ DataKinds
 
 module FrontEnd.Telegram.API where
 
--- import Servant.API
--- import Servant.Client
+import Servant.API
+import Servant.Client
 
--- import Data.Proxy
-import Network.HTTP.Client
--- import Network.HTTP.Types.Status
+import Data.Text as T
+import Data.Proxy
+import Data.Vector
 
 import FrontEnd.Telegram.Data.GetUpdate as GU
--- import FrontEnd.Telegram.Data.SendMessage as SM
+import FrontEnd.Telegram.Data.SendMessage as SM
 
-type BaseUrl = String
--- type Token = String
-
-getUpdate :: BaseUrl -> Manager -> Int -> IO (Maybe Welcome10) -- (Either String Welcome10)
-getUpdate burl m i = do
-  r <- parseRequest $ burl ++ "/getUpdates?offset=" ++ (show i)
-  response <- httpLbs r m
-  return $ GU.decodeTopLevel $ responseBody response
-{-  withResponse r m $ \ response -> do
-    status <- responseStatus response
-    if status == ok200
-       then do
-         fmap (maybe (Left "decode error in getUpdate") Right) (responseBody response >>= GU.decodeTopLevel)
-       else return $ Left (show status)
--}  
-{-
-type TelegramAPI = "getUpdates" :> QueryParam "offset" Int :> Put '[JSON] Welcome10
-              :<|> "sendMessage" :> QueryParam "chat_id" Int :> Put '[JSON] Welcome2
- (QueryParam "chat_id" Int 
-                              :<|> QueryParam "chat_id" String )
-
+type TelegramAPI = "getUpdates" :> QueryParam "offset" Int :> Get '[JSON] Welcome10
+              :<|> "sendMessage" :> QueryParam "chat_id" Int 
+                                 :> QueryParam "text" T.Text 
+                                 :> QueryParam' '[Optional,Strict,JSON] (Vector GU.Entity)
+                                 :> Get '[JSON] Welcome2
+{- (QueryParam "chat_id" Int 
+                                 :<|> QueryParam "chat_id" String )
+-}
 telegramAPI :: Proxy TelegramAPI
 telegramAPI = Proxy
 
 getUpdates :: Maybe Int -> ClientM Welcome10
-sendMessage :: Maybe Int -> ClientM Welcome2
+sendMessage :: Maybe Int -> Maybe T.Text -> ClientM Welcome2
 
 getUpdates :<|> sendMessage = client telegramAPI
--}
--- src/FrontEnd/Telegram/Telegram.hs
+++ src/FrontEnd/Telegram/Telegram.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 
--- | The tekegram front-end
+-- | The console front-end is responsible for console I/O and
+-- appropriate handling of other high-level bot interactions (menu
+-- output etc).
 module FrontEnd.Telegram.Telegram where
 
 --import Servant.API
--- import Servant.Client
+import Servant.Client
 
 import Network.HTTP.Client
+import Network.HTTP.Client.TLS
 
 import qualified Data.Text as T
 --import qualified Data.Text.Read as T
@@ -15,27 +18,39 @@ import qualified System.IO as SIO
 
 -- import qualified FrontEnd.Telegram.Data.GetUpdate as Data
 import qualified FrontEnd.Telegram.API as API
--- import qualified EchoBot
+import qualified EchoBot
+
+data Handle = Hendle
+  { configTelegramBot :: Config
+  , handleEchoBot :: EchoBot.Handle () AccountMessage
+  }
+
+data AccountMessage = AccountMessage
+  { accountMesageText :: T.Text
+  , accountMesageID :: Int
+  , accountEntitiesMessage :: Maybe (Vector Entity )
+  }
+
+data AccountEvent = AccountEvent
+  { accountChatId :: Int
+  , accountFirstName :: T.Text
+  , accountMessages :: Vector AccountMessage
+  }
 
 data Config = Config
   { confBotToken :: T.Text -- confManager :: 
   } deriving Show
-{-
+
 clientEnvDefault :: Config -> IO ClientEnv
 clientEnvDefault conf = do 
-  m <- newManager defaultManagerSettings
+  m <- newManager tlsManagerSettings -- defaultManagerSettings
   url <- parseBaseUrl $ "https://api.telegram.org/bot" ++ (T.unpack $ confBotToken conf)
   return $ mkClientEnv m url
--}
+
 testUpdate' :: Config -> IO () -- (Either ClientError Data.Welcome10)
 testUpdate' conf = do
-  m <- newManager defaultManagerSettings
-  e <- API.getUpdate 
-    ("https://api.telegram.org/bot" ++ (T.unpack $ confBotToken conf) ) 
-    m 
-    1
-{- runClientM (do
+  m <- clientEnvDefault conf
+  e <- runClientM (do
     API.getUpdates (Just 1)
     ) m
--}
   SIO.print e 
